4a781d325b0205a7b2c443f7088c6274382bae78
diloncarnie <diloncarnie@gmail.com>
2025-03-22 00:37:50 +0400

    Modified .gitignore

--- build/rvio2/CMakeFiles/rvio2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rvio2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rvio2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
